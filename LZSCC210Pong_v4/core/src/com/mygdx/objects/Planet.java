c4d5a470edc24be2fd657781455fda32b057e0d3
4nmus <andr_cyp@proton.me>
2025-02-27 14:18:19 +0100

    planet class added

---
+++ LZSCC210Pong_v4/core/src/com/mygdx/objects/Planet.java
@@ -0,0 +1,34 @@
+package com.mygdx.objects;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Blending;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.mygdx.helpers.BodyHelper;
+import com.mygdx.helpers.Constants;
+import com.mygdx.helpers.ContactType;
+import com.mygdx.pong.PongGame;
+import com.mygdx.screens.GameScreen;
+import com.mygdx.pong.PongGame;
+
+public class Planet extends InteractiveObject{
+
+    private String name;
+    private Type type;
+
+    enum Type {
+        Gas,
+        Mineral,
+        Organic
+    }
+
+    public Planet(String name, String type) {
+        this.name = name;
+        this.type = Type.valueOf(type)
+    }
+
+}
