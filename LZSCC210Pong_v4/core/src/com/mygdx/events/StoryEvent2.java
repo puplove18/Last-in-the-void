a692a0b6a8d316b706246044e8605fa71e5997b3
Joseph Seamark <j.seamark@lancaster.ac.uk>
2025-05-06 14:06:59 +0200

    Completed story events

--- LZSCC210Pong_v4/core/src/com/mygdx/events/StoryEvent2.java
+++ LZSCC210Pong_v4/core/src/com/mygdx/events/StoryEvent2.java
@@ -4,8 +4,23 @@ import com.mygdx.objects.Event;
 import com.mygdx.objects.Player;
 
 public class StoryEvent2 extends Event {
-    
+
     public StoryEvent2() {
-        super("Temp", "Temp");
+        super("Cosmic Energy", 
+            """
+            Your ship's sensors detect an unusual energy signature ahead. The energy
+            doesn't appear harmful, but it's completely unknown to your systems.
+            """);
+        
+        addChoice("Study the energy", 100, player -> {
+            setSuccessMessage("""
+                You move closer. The readings are extraordinary. 
+                The energy seems to exist outside normal space-time. 
+                
+                """);
+        },
+        player -> {
+        });
+        
     }
 }
